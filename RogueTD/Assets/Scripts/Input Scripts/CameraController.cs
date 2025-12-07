6a6e12b82467f786a739bf11ba7d9606fd20c782
Sp1ke <97825185+SpikeTheSpider@users.noreply.github.com>
2025-12-07 18:12:25 +0500

    Camera boundries

--- RogueTD/Assets/Scripts/Input Scripts/CameraController.cs
+++ RogueTD/Assets/Scripts/Input Scripts/CameraController.cs
@@ -8,13 +8,20 @@ public class CameraController : MonoBehaviour
     [SerializeField] float minZoom = 2f;
     [SerializeField] float maxZoom = 10f;
     [SerializeField] Camera cam;
+    [SerializeField] MapManager mapManager;
+    
+    [SerializeField] private float minX = -10f;
+    [SerializeField] private float maxX = 10f;
+    [SerializeField] private float minY = -10f;
+    [SerializeField] private float maxY = 10f;
     
     private GameInput _gameInput;
     private Vector3 _targetPosition;
+    private float _mapWidth;
+    private float _mapHeight;
     
     private void Start()
     {
-        
         _gameInput = GameInputManager.Instance.GameInput;
         if (cam == null)
             cam = GetComponent<Camera>();
@@ -29,6 +36,7 @@ public class CameraController : MonoBehaviour
         ReadMovement();
         ReadZoom();
         ApplyMovement();
+        ApplyZoom();
     }
 
     private void ReadMovement()
@@ -37,7 +45,9 @@ public class CameraController : MonoBehaviour
         
         if (inputDirection != Vector2.zero)
         {
-            _targetPosition += new Vector3(inputDirection.x, inputDirection.y, 0) * (cameraSpeed * Time.deltaTime);
+            Vector3 newPosition = _targetPosition + new Vector3(inputDirection.x, inputDirection.y, 0) * (cameraSpeed * Time.deltaTime);
+            
+            _targetPosition = ClampCameraPosition(newPosition);
         }
     }
 
@@ -59,4 +69,21 @@ public class CameraController : MonoBehaviour
             cam.orthographicSize = Mathf.Clamp(newSize, minZoom, maxZoom);
         }
     }
+    
+    private void ApplyZoom()
+    {
+        _targetPosition = ClampCameraPosition(_targetPosition);
+    }
+
+    private Vector3 ClampCameraPosition(Vector3 position)
+    {
+        var cameraHeight = cam.orthographicSize;
+        var cameraWidth = cameraHeight * cam.aspect;
+        
+        var clampedX = Mathf.Clamp(position.x, minX + cameraWidth, maxX - cameraWidth);
+        var clampedY = Mathf.Clamp(position.y, minY + cameraHeight, maxY - cameraHeight);
+        
+        return new Vector3(clampedX, clampedY, position.z);
+    }
+    
 }
